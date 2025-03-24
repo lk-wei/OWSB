99bb5788e8f996cc19b77c6c8181804d9601c8ac
lk-wei <limkangwei25@gmail.com>
2025-03-25 00:02:45 +0800

    progress

--- src/main/java/repository/UserRepo.java
+++ src/main/java/repository/UserRepo.java
@@ -4,10 +4,49 @@
  */
 package repository;
 
+import domain.*;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+
 /**
  *
  * @author Kang Wei
  */
 public class UserRepo {
+    final private Path filePath;
     
+    public UserRepo(String filePath){
+        this.filePath = Path.of(filePath);
+    }
+    
+    // create
+    public void createUser(User user) throws IOException{
+         List<String> lines = Files.readAllLines(filePath);
+         lines.add(objectToString(user));
+         
+         Files.write(filePath, lines);
+    }
+    
+    // read
+    
+    
+    // update
+    
+    
+    // delete
+    
+    // others
+    private String objectToString(User user){
+        return String.join(
+                user.getUserId(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getRole()
+        );
+    }
+    
+    private User stringToObject(String line){
+        return new User()
+    }
 }
