3840062fe1b3cb9bede242dad1c144163e97167b
lk-wei <limkangwei25@gmail.com>
2025-05-30 23:49:55 +0800

    Addee alerat Repo

--- src/main/java/repository/AlertRepo.java
+++ src/main/java/repository/AlertRepo.java
@@ -5,7 +5,13 @@
 package repository;
 
 import domain.Alert;
+import domain.User;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -13,12 +19,49 @@ import java.nio.file.Path;
  */
 public class AlertRepo extends MasterRepo<Alert>{
 
-    public AlertRepo(Path filePath) {
-        super(filePath);
+    public AlertRepo() {
+        super(Path.of("database/alert.txt"));
     }
     
     // Custom Method
+    // UI method
+    public DefaultTableModel getTableModel(String currentUserRole) throws IOException {
+        DefaultTableModel model = new DefaultTableModel(
+            new Object[][]{},
+            // These column names must match what's in your JFrame
+            new String[]{"From", "Title", ""}
+        );
+
+        List<Alert> alerts = getByReciverRole(currentUserRole); 
+        UserRepo userRepo = new UserRepo();    
+
+        for (Alert alert : alerts) {
+            User sender = userRepo.getUserById(alert.getSender());
+            
+            model.addRow(new Object[]{
+                sender.getUserName(),    // Sender Name
+                alert.getTitle(),   // Title
+                ""                        // Empty column (action buttons?)
+            });
+        }
+        return model;
+    }
     
+    public List<Alert> getByReciverRole(String role) throws IOException{
+        List<Alert> list = new ArrayList<>();
+        List<String> lines = Files.readAllLines(filePath);
+
+        for (String line : lines) {
+            Alert alert = stringToObject(line);
+            
+            if(alert.getReciverRole().equals(role) || "AD".equals(role)){
+                 list.add(alert);
+            }
+            
+            
+        }
+        return list;
+    }
     
     // Implement required abstract methods
     @Override
